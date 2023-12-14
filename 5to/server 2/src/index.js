0e211fa35b73ba4a01659b033a69413eb7ecf09f
Ismael Medrano <ismaelmed500@gmail.com>
2023-12-14 11:18:41 -0300

    trbajo en clases

--- 5to/server 2/src/index.js
+++ 5to/server 2/src/index.js
@@ -1,9 +1,34 @@
 import express from "express";
 import { dirname , join} from 'path'
 import { fileURLToPath } from "url";
+// npx nodemon src/index.js
 import path from "path";
 
-// npx nodemon src/index.js
+import { MongoClient, ServerApiVersion } from "mongodb"
+const uri = "mongodb+srv://ismaelmed500:<ismaelmed500>@cluster0.xz7sze9.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 import indexRoutes from './routes/routes.js' //importa las rutas desde routes.js
 
